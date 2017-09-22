962bc816514ae2e5de1fcbad3c9869593bf384d3
Eirik S. Norheim <noreirik@gmail.com>
2017-09-22 23:38:30 +0200

    ex1 - implemented single variable gradient descent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -6,6 +6,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
+scale = alpha/m;
 
 for iter = 1:num_iters
 
@@ -16,12 +17,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    
+    hypothesis = X * theta;
+    hypothesis_minus_y = hypothesis - y;
+    theta(1) = theta(1) - scale * (hypothesis_minus_y' * X(:,1));
+    theta(2) = theta(2) - scale * (hypothesis_minus_y' * X(:,2));
 
     % ============================================================
 
